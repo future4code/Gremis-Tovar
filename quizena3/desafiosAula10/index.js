796f796f24f89a8f92f0721d25bd0132c146e71d
gremis <18993990tovargremis@gmail.com>
2021-04-01 13:59:59 -0300

    challenge finished

--- quizena3/desafiosAula10/index.js
+++ quizena3/desafiosAula10/index.js
@@ -37,3 +37,61 @@ console.log("O valor a ser pago com 15% de desconto é " + priceHourDescont + " 
 //O valor a ser pago é 14 R$ por 280 quilowatt-hora
 //O valor a ser pago com 15% de desconto é 11.9 R$
 
+//3.a Procure uma forma de converter libra (lb) para quilograma (kg) e escreva um programa que converta 20lb para kg.
+let lb = parseFloat(prompt("Digite uma quantidade em libras"))
+let kg = lb/2.205
+let kgReal= kg.toFixed(2)
+
+console.log(lb + " lb, equivalem a " + kgReal + " kg")
+
+//20 lb, equivalem a 9.07 kg
+
+// 3.b. Procure uma forma de converter onça (oz) para quilograma (kg) e escreva um programa que converta 10.5oz para kg
+
+let oz = parseFloat(prompt("Digite uma quantidade em onças"))
+let convertKg = oz/35.274
+let convertKgReal= convertKg.toFixed(2)
+
+console.log(oz + " oz, equivalem a aproximadamente " + convertKgReal + " kg")
+
+//10.5 oz, equivalem a 0.30 kg
+
+// 3.c. Procure uma forma de converter milha (mi) para metro (m) e escreva um programa que converta 100mi para m
+
+let mi = parseFloat(prompt("Digite uma quantidade em milhas"))
+let metro = mi*1609
+let metroReal= metro.toFixed(2)
+
+console.log(mi + " mi, equivalem a aproximadamente " + metroReal + " m")
+
+//100 mi, equivalem a 160900.00 m
+
+//3.d. Procure uma forma de converter pés (ft) para metro (m) e escreva um programa que converta 50ft para m.
+
+let ft = parseFloat(prompt("Digite uma quantidade em pés"))
+let mt = ft/3.281
+let mtReal= mt.toFixed(2)
+
+console.log(ft + " ft , equivalem a aproximadamente " + mtReal + " m")
+
+//50 ft , equivalem a 15.24 m
+
+//3.e. Procure uma forma de converter galão (gal) para litro (l) e escreva um programa que converta 103.56gal para litro.
+
+let gal = parseFloat(prompt("Digite uma quantidade em galão"))
+let litro = gal*4.4049
+let litroReal= litro.toFixed(2)
+
+console.log(gal + " gal , equivalem a aproximadamente " + litroReal + " lt")
+
+//103.56 gal , equivalem a aproximadamente 456.17 lt
+
+//3.f. Procure uma forma de converter xícara (xic) para litro (l) e escreva um programa que converta 450xic para litro.
+
+let xic = parseFloat(prompt("Digite uma quantidade em xícara"))
+let lt = xic*0.236588
+let ltReal= lt.toFixed(2)
+
+console.log(xic + " xic , equivalem a aproximadamente " + ltReal + " lt")
+
+//450 xic , equivalem a aproximadamente 106.45 lt
