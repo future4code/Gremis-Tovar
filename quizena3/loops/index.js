ccb5184e25b9ff8cd23bf07b65a72bb023c8dc69
gremis <18993990tovargremis@gmail.com>
2021-04-08 10:56:24 -0300

    completed exercises

---
+++ quizena3/loops/index.js
@@ -0,0 +1,162 @@
+/*Exercicios de Interpretação */
+
+//EXERCÍCIO 1_O que o código abaixo está fazendo? Qual o resultado impresso no console?
+
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+
+// Declara una variável com valor inicial de 0 antes de começar o ciclo
+// Dentro do for está a condição para executar o ciclo, neste caso o index deve ser executado até que seja menor a 5
+// Na terceira declaração a variavél let vai ser redeclarada conforme a condição acima e vai ir sendo sumada ao i
+//  valor = valor + i
+// Então no primeiro ciclo                  -> valor = 0 + 1 = 1
+// No segundo ciclo                         -> valor = 1 + 2 = 3
+// Terceiro ciclo                           -> valor = 3 + 3 = 6
+// E no último ciclo que é i < 5 = 4, seria -> valor = 6 + 4 = 10
+// Por isso o valor impresso no console é 10, ou seja, está fazendo uma iteração do valor 4 vezes.
+
+//EXERCÍCIO 2_Leia o código abaixo:
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+
+// a. O que vai ser impresso no console?
+// [19, 21, 23, 25, 27, 30] os números menor do que 18.
+// b. Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...` é suficiente? Se sim, o que poderia ser usado para fazer isso?
+// Sim seria suficiente
+// Agradeço a este link de stackoverflow pela resposta
+// https://stackoverflow.com/questions/34348937/access-to-es6-array-element-index-inside-for-of-loop
+// E seria desse jeito:
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let [index, numero] of lista.entries()) {
+//   if (numero > 18) {
+// 		console.log("index: " + index + "\n" + "valor: " + numero)
+// 	}
+// }
+
+/*DESAFIO 1_Qual seria o resultado impresso no console, se o usuário digitasse o número `4` ?*/
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "0"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+
+// O laço while faz que o programa imprima 4 vezes (se o usuário digitasse o número 4) a variável linha
+// Enquanto quantidadeAtual seja menor do que 4 (o número que o usuário digite), imprime a `linha`
+// 0 < 1 -> imprime a linha
+// 0 < 2 -> imprime a linha
+// 0 < 3 -> imprime a linha
+// 0 < 4 -> imprime a linha
+// Vamos ter então 4 linhas, o que vai ir dentro da linha vai ser definido pelo laço for que está dentro do while
+// Ao inicio se declara a variável vazia, mas vai estar esperando um valor tipo string
+// que vai executar iterações enquanto 0 (asteriscos) seja menor quantidadeAtual + 1, ou seja, 4 vezes
+//  linha = "linha + "0""
+// Então no primeiro ciclo                  -> linha = "" + "0" = "0"
+// No segundo ciclo                         -> linha = "0" + "0" = "00"
+// Terceiro ciclo                           -> linha = "00" + "0" = "000"
+// E no último ciclo, seria                 -> linha = "000" + "0" = "0000"
+//Como são string vão ir se juntando em uma linha só
+
+
+/*Exercicios de Escrita de Código */
+
+// EXERCÍCIO 3_ Nas perguntas abaixo, considere que você tenha acesso a um `array`  (chamado de 'array original') que seja composto somente de números. Após o enunciado, há um exemplo de qual deve ser a resposta final de cada programa individualmente.
+
+// const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55, 90, 40, 150, 50, 70, 31, 80, 180, 100, 193, 170, 65]
+
+
+// a. Escreva um programa que:
+
+// - **Imprima** cada um dos valores do array original.
+
+// for(let i = 0; i < array.length; i++) {
+//     console.log(array[i])
+// }
+
+// b. Escreva um programa que:
+
+// - **Imprima** cada um dos valores do array original divididos por 10
+
+// for(let i = 0; i < array.length; i++) {
+//     console.log(array[i]/10)
+// }
+
+// c. Escreva um programa que:
+
+// - **Crie** um novo array contendo, somente, os números pares do array original.
+// let newArray=[]
+// for(let i = 0; i < array.length; i++) {
+//     if(array[i] % 2 == 0){
+//         newArray.push(array[i])
+//     }
+// }
+//- **Imprima** esse novo array
+// console.log(newArray)
+
+// d. Escreva um programa que:
+
+// - **Crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`"
+
+// for (let [index, numero] of array.entries()) {
+//     // - **Imprima** este novo array
+//     console.log("O elemento do índex : " + index + " é: " + numero)
+// }
+
+
+// e. Escreva um programa que imprima no console o maior e o menor números contidos no array original
+
+// let valorMaximo = 0;
+// let valorMinimo = array[0]; //Pelo momento o mínimo sera o primeiro número do nosso array
+
+// let valorMinimo = Math.min(...array) ; O valor minimo se pode achar só fazendo esta função
+
+// for(let i = 0; i < array.length; i++) {
+//     if(array[i] > valorMaximo){
+//         valorMaximo = array[i]
+//     } else if (array[i] < valorMinimo){
+//         valorMinimo = array[i]
+//     }
+// }
+
+// console.log("O maior número e: ", valorMaximo)
+// console.log("O menor número e: ", valorMinimo)
+
+
+/*DESAFIOS*/
+
+// DESAFIO 1_Neste exercício vocês vão implementar uma brincadeira muito simples: "Adivinhe o número que estou pensando". Ele deve ser jogado entre duas pessoas. Inicialmente, uma das pessoas insere qual o número em que ela pensou. A outra pessoa tem que ficar chutando até acertar em cheio o número. Esta é uma tarefa difícil, então quem escolheu o número fica dando umas dicas para a outra pessoa, indicando se o número que ela pensou é maior ou menor do que o chute em si. Veja, abaixo, um exemplo de partida:
+
+// Vamos jogar!
+// O número chutado foi: 3
+// Errrrrrrrou, é maior
+// O número chutado foi: 18
+// Errrrrrrrou, é menor
+// O número chutado foi: 15
+// Errrrrrrrou, é menor
+// O número chutado foi: 11
+// Acertou!!
+// O número de tentativas foi: 4 
+
+
+// Um resumo das funcionalidades são:
+
+// - Solicitar que o primeiro jogador escolha um número, através do `prompt`. Neste momento, deve-se imprimir no console a mensagem `Vamos jogar!`
+// - A partir daí, será solicitado, ao segundo jogador, que ele chute os números até acertar, através do `prompt`. A cada chute, deve ser informado no console:
+//     - O número chutado, com a mensagem: `O número chutado foi: <número>`
+//     - Uma mensagem dizendo se o número escolhido é maior ou menor do que o número chutado: `Errou. O número escolhido é maior/menor`
+// - Quando o segundo jogador acertar o número escolhido pelo primeiro jogador, deve ser impressa a mensagem: `Acertou` ; e, embaixo, `O número de tentativas foi : <quantos chutes o usuário deu>`
+
