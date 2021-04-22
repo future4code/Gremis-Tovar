eb59b5bab46c164410b49dee4bf9e1cf7ec044b2
gremis <18993990tovargremis@gmail.com>
2021-04-22 18:44:19 -0300

    corretions

--- quinzena4/lista-de-exercicios/testes.js
+++ quinzena4/lista-de-exercicios/testes.js
@@ -27,11 +27,13 @@ const arrayCompare = (arr1, arr2) => {
   }
   for(let i = 0; i < arr1.length; i++) {
     if (isObject(arr1[i])) {
-      return objectCompare(arr1[i], arr2[i])
-    } else if (arr1[i] !== arr2[i]) {
+      if (!objectCompare(arr1[i], arr2[i])) {
         return false
       }
+    } else if (arr1[i] !== arr2[i]) {
+      return false
     }
+  }
   return true
 }
 
