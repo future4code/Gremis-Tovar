18439b67ba158e39b33804d07b59b902dd1db53b
gremis <18993990tovargremis@gmail.com>
2021-04-29 12:22:05 -0300

    exercise ended

--- quinzena5/labedin/src/components/CardGrande/CardGrande.js
+++ quinzena5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import './CardGrande.css'
+import React from "react";
+import styled from "styled-components";
+
+const BigcardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border: 1px solid black;
+  padding: 20px 30px;
+  margin-bottom: 10px;
+  height: 200px;
+`;
+
+const BigcardContainerImg = styled.img`
+  width: 100px;
+  margin-right: 20px;
+`;
+
+const Title = styled.h4`
+  margin-bottom: 15px;
+`;
 
 function CardGrande(props) {
-    return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
-    )
+  return (
+    <BigcardContainer>
+      <BigcardContainerImg src={props.imagem} />
+      <div>
+        <Title>{props.nome}</Title>
+        <p>{props.descricao}</p>
+      </div>
+    </BigcardContainer>
+  );
 }
 
 export default CardGrande;
+
+
+// function CardGrande(props) {
+//     return (
+//         <div className="bigcard-container">
+//             <img src={ props.imagem } />
+//             <div>
+//                 <h4>{ props.nome }</h4>
+//                 <p>{ props.descricao }</p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default CardGrande;
--- quinzena5/labedin/src/components/CardPequeno/CardPequeno.js
+++ quinzena5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,17 +1,63 @@
-import React from 'react'
-import './CardPequeno.css'
+import React from "react";
+import styled from "styled-components";
 
+const SmallcardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  border: 1px solid black;
+  padding: 20px 30px;
+  margin-bottom: 10px;
+  height: 70px;
+`;
+
+const Line = styled.div`
+  display: inline-flex;
+  padding: 20px;
+  margin-top: 10px;
+`;
+
+const SmallcardContainerImg = styled.img`
+  width: 35px;
+`;
+
+const Title = styled.h4`
+  margin-bottom: 15px;
+`;
+
+const Paragrafo = styled.p`
+  margin-left: 5px;
+`;
 
 function CardPequeno(props) {
-    return (
-        <div className="smallcard-container">
-            <img src={ props.imagem } />
-            <div className="line">
-                <h4>{ props.titulo } </h4>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
-    )
+  return (
+    <SmallcardContainer>
+      <SmallcardContainerImg src={props.imagem} />
+      <Line>
+        <Title>{props.titulo} </Title>
+        <Paragrafo>{props.descricao}</Paragrafo>
+      </Line>
+    </SmallcardContainer>
+  );
 }
 
-export default CardPequeno
+export default CardPequeno;
+
+
+
+
+
+// function CardPequeno(props) {
+//     return (
+//       <div className="smallcard-container">
+//         <img src={props.imagem} />
+//         <div className="line">
+//           <h4>{props.titulo} </h4>
+//           <p>{props.descricao}</p>
+//         </div>
+//       </div>
+//     );
+//   }
+  
+//   export default CardPequeno;
+  
