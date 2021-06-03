6d812bb6f92cdac2d665d858df76aa8f0618ab9c
Gremis Tovar <18993990tovargremis@gmail.com>
2021-06-03 07:25:16 -0300

    swapi part 3 person list

--- quinzena7/swapi/src/pages/CharacterListPage/CharacterListPage.js
+++ quinzena7/swapi/src/pages/CharacterListPage/CharacterListPage.js
@@ -1,7 +1,28 @@
 import React from "react";
+import axios from "axios";
+import { BASE_URL } from "../../constants/url";
+import { CharacterCard } from "./styled";
 
 export default class CharacterListPage extends React.Component {
-    render(){
-        return <div>List</div>
-    }
+  state = {
+    characterList: [],
+  };
+
+  componentDidMount() {
+    this.getCharacterList();
+  }
+
+  getCharacterList = () => {
+    axios
+      .get(`${BASE_URL}/people`)
+      .then((res) => this.setState({ characterList: res.data.results }))
+      .catch((err) => console.error(err));
+  };
+
+  render() {
+    const characters = this.state.characterList.map((person) => {
+      return <CharacterCard key={person.url}>{person.name}</CharacterCard>;
+    });
+    return <p>{characters}</p>;
+  }
 }
