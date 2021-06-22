002f99f17a3370d159040a4f1472048c30dc0aa3
Gremis Tovar <18993990tovargremis@gmail.com>
2021-06-22 11:34:59 -0300

    start labex structure

---
+++ quinzena9/labex/src/globalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+`;
+
+export default GlobalStyle;
---
+++ quinzena9/labex/src/routes/Router.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Router = () => {
+    return (
+        <div>
+            Hola
+        </div>
+    )
+}
+
+export default Router
